f2c525650ad4cdfc33a2e600f756c22018f9b68d
Tommaso Lamma <lammatommaso@gmail.com>
2021-07-15 16:57:06 +0200

    default

--- cpp/simulator.cpp
+++ cpp/simulator.cpp
@@ -84,7 +84,7 @@ Node Simulator::_find_next(int car_index)const
     return next_node;
 }
 
-Node Simulator::_find_second_next(int car_index)const
+Node Simulator::_find_second_next(int car_index)const //-----------> forse in realtà è qui il problema!
 {
     bool search_end = false;
     Node next_node;
@@ -151,7 +151,7 @@ void Simulator::_mv_car(int car_index)
                 if(car_in_front == _find_road(i).get_width()) break;
             }
         }
-        if(car_in_front < _find_next_road(i).get_width()) //-------> è qui il problema!!!!!!!!
+        if(car_in_front < _find_next_road(car_index).get_width()) //-------> è qui il problema!!!!!!!!
         {
             _car_vector[car_index].position = next_node;
             _car_vector[car_index].car->move_forward();
